059abe956e63cc0a2c6961726dfdfdc366b12b80
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-01-20 09:00:24 -0600

    edit

--- server.js
+++ server.js
@@ -1,120 +1,191 @@
-// Build a command-line application that at a minimum allows the user to:
-//   * View departments, roles, employees.
-//   * Add departments, roles, employees.
-//   * Update employee roles.
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
+const dotenv = require("dotenv");
+dotenv.config();
 
-const inquirer = require("inquirer");
-const mysql = require("mysql");
-const consoletable = require("console.table");
-
-const connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host: "localhost",
+
+    // Your port; if not 3306
     port: 3306,
+
+    // Your username
     user: "root",
+
+    // Your password
     password: "Trainstogo1@",
     database: "employeetracker_DB"
-  });
+});
 
-  connection.connect(function(err) {
+
+connection.connect(function (err) {
     if (err) throw err;
-    starter();
-  });
+    runSearch();
+});
 
-//__________________________________________________________
 
-function starter() {
+function runSearch() {
     inquirer
         .prompt({
             name: "action",
-            type: "list",
+            type: "rawlist",
             message: "What would you like to do?",
             choices: [
+                "Add departments",
+                "Add roles",
+                "Add employees",
                 "View departments",
-                "view employees",
-                "view roles",
-                "Add a department",
-                "Add an employee",
-                "Add a role",
-                "Update an employee's role",
-                "Exit"
+                "View roles",
+                "View employees",
+                "Update employee roles"
             ]
         })
-        .then(function(answer) {
+        .then(function (answer) {
             switch (answer.action) {
-            case "View departments":
-                viewdpartments();
-                break;
+                case "Add departments":
+                    addDepartments();
+                    break;
 
-            case "view employees":
-                viewEmployees();
-                break;
+                case "Add roles":
+                    addRoles();
+                    break;
 
-            case "view roles":
-                viewroles();
-                break;
+                case "Add employees":
+                    addEmployees();
+                    break;
 
-            case "Add a department":
-                addDepartment();
-                break;
+                case "View departments":
+                    viewDepartments();
+                    break;
 
-            case "Add an employee":
-                addEmployee();
-                break;
+                case "View roles":
+                    viewRoles();
+                    break;
 
-            case "Add a role":
-                addRole();
-                break;
+                case "View employees":
+                    viewEmployees();
+                    break;
 
-            case "Update an employee's role":
-                updateEmployeeRole();
-                break;
-            
-            case "Exit":
-                connection.end();
-                break;
+                case "Update employee roles":
+                    updateEmployeeRoles();
+                    break;
             }
-        })
-
-
+        });
 }
 
-//__________________________________________________________
-function viewdpartments(){
-    var query = "SELECT * FROM department";
-    connection.query(query, function(err, res) {
-        if (err) throw err;
+function addDepartments() {
+    inquirer
+        .prompt({
+            type: "input",
+            name: "department",
+            message: 'What is the name of the department?'
+        })
+        .then((function (answer) {
+            console.log(answer);
+            var query = "INSERT INTO departments (department_name) VALUES ?";
+            var values = [[answer.department]]
+            connection.query(query, [values], function (err, res) {
+                console.log(res);
+                runSearch();
+            });
+        }))
+}
+
+function addRoles() {
+    var query = "SELECT department_name as name, id as value FROM departments";
+    connection.query(query, function (err, departments) {
+        inquirer
+            .prompt([{
+                type: "input",
+                name: "title",
+                message: 'What is the role title?'
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: 'What is the salary?'
+            },
+            {
+                type: "list",
+                name: "departmentId",
+                message: "What is the department name?",
+                choices: departments
+            }
+            ])
+            .then((function (answer) {
+                console.log(answer);
+                var query = "INSERT INTO roles (title,salary,department_id) VALUES ?";
+                var values = [[answer.title, answer.salary, answer.departmentId]]
+                connection.query(query, [values], function (err, res) {
+                    console.log(res);
+                    runSearch();
+                });
+            }))
+    });
+}
+
+function addEmployees() {
+    var query = "SELECT title as name, id as value FROM roles";
+    connection.query(query, function (err, roles) {
+        inquirer
+            .prompt([{
+                type: "input",
+                name: "firstName",
+                message: 'What is the first name?'
+            },
+            {
+                type: "input",
+                name: "lastName",
+                message: 'What is the last name?'
+            },
+            {
+                type: "list",
+                name: "roleId",
+                message: "What is the role title?",
+                choices: roles
+            }
+            ])
+            .then((function (answer) {
+                console.log(answer);
+                var query = "INSERT INTO employees (first_name,last_name,role_id) VALUES ?";
+                var values = [[answer.firstName, answer.lastName, answer.roleId]]
+                connection.query(query, [values], function (err, res) {
+                    console.log(res);
+                    runSearch();
+                });
+            }))
+    });
+}
+
+function viewDepartments() {
+    var query = "SELECT * FROM departments";
+    connection.query(query, function (err, res) {
         console.table(res);
-        starter();
-    })
-    //id
-    //department_name
-};
+        runSearch();
+    });
+}
 
-function viewroles(){
-    var query = "SELECT * FROM role";
-    connection.query(query, function(err, res) {
-        if (err) throw err;
+function viewRoles() {
+    var query = "SELECT * FROM roles";
+    connection.query(query, function (err, res) {
         console.table(res);
-        starter();
-    })
-};
+        runSearch();
+    });
+}
 
-function viewEmployees(){
-    var query = "SELECT * FROM employee";
-    connection.query(query, function(err, res) {
-        if (err) throw err;
+function viewEmployees() {
+    var query = "SELECT * FROM employees";
+    connection.query(query, function (err, res) {
         console.table(res);
-        starter();
-    })
-};
+        runSearch();
+    });
+}
 
-function addEmployee(){};
-
-function addDepartment(){};
-
-function addRole(){};
-
-//__________________________________________________________
-
-function updateEmployeeRole(){};
+function updateEmployeeRoles() {
+    var query = "SELECT * FROM roles";
+    connection.query(query, function (err, res) {
+        console.table(res);
+        runSearch();
+    });
+}
